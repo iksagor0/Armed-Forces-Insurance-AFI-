26ad9feaa87cd607330bffaa41ae1114602e8ad3
iksagor0 <iksagor0@gmail.com@gmail.com>
2023-04-17 12:47:06 +0600

    add bootstrap row,col in card-group

--- AFI/Home/js/card-group.js
+++ AFI/Home/js/card-group.js
@@ -3,23 +3,39 @@
   /* <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>; */
 }
 
-if (screen.width <= 768) {
-  let swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
+if (screen.width < 768) {
+  const slider = document.querySelectorAll(".swiper-wrapper .col-md-3");
+  // Remove Row Class
+  const gridRow = document.querySelector(".row.swiper-wrapper");
+  console.log(gridRow);
+  gridRow.classList.remove("row");
 
-    breakpoints: {
-      450: {
-        slidesPerView: 1,
-        spaceBetween: 40,
+  // Add Swiper Class
+  slider.forEach((element) => {
+    element.classList.add("swiper-slide");
+    console.log(element);
+  });
+
+  // Initiate Slider
+  window.addEventListener("load", () => {
+    const swiper = new Swiper(".mySwiper", {
+      spaceBetween: 30,
+      // autoplay: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
       },
-      768: {
-        slidesPerView: 1,
-        spaceBetween: 40,
+
+      breakpoints: {
+        450: {
+          slidesPerView: 1,
+          spaceBetween: 40,
+        },
+        768: {
+          slidesPerView: 1,
+          spaceBetween: 40,
+        },
       },
-    },
+    });
   });
 }
