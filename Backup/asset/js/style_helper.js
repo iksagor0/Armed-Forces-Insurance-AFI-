7311c82efd4966db6ccb810232516812a98854c1
iksagor0 <iksagor0@gmail.com>
2023-05-18 21:06:47 +0600

    AFI asset 18 May v7

--- Backup/asset/js/style_helper.js
+++ Backup/asset/js/style_helper.js
@@ -25,6 +25,13 @@ addClassToParent("image_link__wrapper_item", "image_link__grid_container");
 addClassToParent("grid__article_aside", "grid__article_9-3");
 addClassToParent("item_75", "parent_75_25");
 addClassToParent("life_logo_image", "life_logo_image_parent");
+addClassToParent("home_blue_card_item", "home_blue_cards_grid");
+addClassToParent("card_group_item", "card_group_grid");
 
 // To make 50-50 grid column
 parent50x50("left__text__content", "TypesOfBusiness");
+
+// Fix big screen alignment issues with #content > first .row
+document
+  .getElementById("content")
+  ?.firstElementChild?.classList.add("flex-column");
