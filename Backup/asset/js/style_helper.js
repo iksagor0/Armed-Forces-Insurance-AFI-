f9b3ea353d4509f1c6788205325258bea087f41e
iksagor0 <iksagor0@gmail.com>
2023-05-21 21:13:22 +0600

    update Live Assets

--- Backup/asset/js/style_helper.js
+++ Backup/asset/js/style_helper.js
@@ -22,6 +22,7 @@ addClassToParent("related_article__item", "related_article__container");
 addClassToParent("policy_cover_item", "policy_cover_container");
 addClassToParent("icon_item", "icon_wrapper");
 addClassToParent("image_link__wrapper_item", "image_link__grid_container");
+addClassToParent("image_link__wrapper_item_two", "image_link__grid_container");
 addClassToParent("grid__article_aside", "grid__article_9-3");
 addClassToParent("item_75", "parent_75_25");
 addClassToParent("life_logo_image", "life_logo_image_parent");
@@ -35,3 +36,14 @@ parent50x50("left__text__content", "TypesOfBusiness");
 document
   .getElementById("content")
   ?.firstElementChild?.classList.add("flex-column");
+
+//
+function addClassByPagePath(pagePath, classname) {
+  const path = location.pathname.toLocaleLowerCase();
+  if (path === pagePath) {
+    const element = document.querySelector(`.${classname}`);
+    element?.classList.add(`${classname}__css`);
+  }
+}
+
+addClassByPagePath("/articles/auto", "article_insurance_quote");
